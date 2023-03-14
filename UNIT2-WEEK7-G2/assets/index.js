aaeef339d75787403eb96104ba30caea6d3d0c5c
DarioSalvatore <dariopay69@gmail.com>
2023-03-14 16:33:41 +0100

    UNIT2-WEEK7-G3

---
+++ UNIT2-WEEK7-G2/assets/index.js
@@ -0,0 +1,42 @@
+let nome = document.getElementById("nome");
+let salva = document.getElementById("salva");
+let rimuovi = document.getElementById("rimuovi");
+let valore = document.getElementById("valore");
+
+function salvataggio() {
+  const nomesalvato = (salva.value = nome.value);
+  console.log(salva.value);
+  localStorage.setItem("nome:", nomesalvato);
+  nome.value = "";
+}
+
+salva.addEventListener("click", salvataggio);
+rimuovi.addEventListener("click", elimina);
+salva.addEventListener("click", scrivi);
+//-----------fine salvataggio--------------------
+
+//-----------Rimuovere dal local storage----------
+
+function elimina() {
+  localStorage.removeItem("nome:");
+  rimuovititolo();
+  alert("hai rimosso correttamente il valore in localstorage");
+}
+
+window.onload = () => {
+  let appendi = localStorage.getItem("nome:");
+  crea.textContent = appendi;
+  valore.appendChild(crea);
+};
+
+//--------------
+let crea = document.createElement("h2");
+function scrivi() {
+  let appendi = localStorage.getItem("nome:");
+  crea.textContent = appendi;
+  valore.appendChild(crea);
+}
+
+function rimuovititolo() {
+  crea.textContent = "";
+}
